f8d5ad8a183b64b48520ff69dfc44e5038ca14d7
xiaoyuema333 <xma333@wisc.edu>
2024-06-28 04:53:23 -0500

    Completed the TODO part in the changeColor method in Color.java, and added the Makefile

--- Color.java
+++ Color.java
@@ -24,9 +24,12 @@ public class Color{
 	 * 	and different from the exisitng color before making the change
 	 */
 	public boolean  changeColor(String newColor){
+	if (isColorValid(newColor) && !(newColor.equals(this.color))) {
 		this.color = newColor;
-
+                
 		return true;
+	}
+	return false;
 	}
 
 	public boolean isColorValid(String inputColor){
